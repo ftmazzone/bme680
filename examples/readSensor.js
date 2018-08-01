c51cb55b8f01f418992f4f98bba11d22b0132c74
Florent Mazzone <None>
2018-08-01 15:00:16 +0200

    Implement i2c communication

---
+++ examples/readSensor.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { Bme680 } = require('../lib');
+const bme680 = new Bme680('/dev/i2c-1', 0x76);
+
+bme680.initialize().then(async () => {
+    console.info('Sensor initialized');
+    setInterval(async () => {
+        console.info(await bme680.getSensorData());
+    }, 3000);
+});
+
